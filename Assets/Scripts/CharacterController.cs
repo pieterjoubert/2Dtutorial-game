207e61f224e48abe37cb6cffebc4b848b1a3d985
Pieter Joubert <pieter.joubert@gmail.com>
2017-04-05 22:21:53 +0200

    Added platforms,character movement and coins. As well as collision detection for all of the game object

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    private bool airborne = false;
+    public float thrust;
+    public float maxVelocity;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            
+            rb = GetComponent<Rigidbody2D>();
+            if(Mathf.Abs(rb.velocity.x) < maxVelocity)
+                rb.AddForce(transform.right * thrust);
+         
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb = GetComponent<Rigidbody2D>();
+            if (Mathf.Abs(rb.velocity.x) < maxVelocity)
+                rb.AddForce(transform.right * -thrust);
+            
+           
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb = GetComponent<Rigidbody2D>();
+            if (!airborne && Mathf.Abs(rb.velocity.y) < maxVelocity)
+            {
+                rb.AddForce(transform.up * thrust * 20f);
+                airborne = true;
+            }
+        }
+
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "platform")
+           airborne = false;
+
+        if (coll.gameObject.tag == "ramp")
+        {
+            rb = GetComponent<Rigidbody2D>();
+            if (Mathf.Abs(rb.velocity.y) < maxVelocity)
+                rb.AddForce(transform.up * thrust * 40f);
+           
+        }
+
+        if (coll.gameObject.tag == "coin")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
